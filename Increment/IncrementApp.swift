22044ff8390de986ba8a92b2afabe1680e8f0811
wesley brito <wesleypkn@gmail.com>
2020-08-19 11:21:34 -0300

    Initial Commit

---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementApp.swift
+//  Increment
+//
+//  Created by Wesley Brito on 19/08/20.
+//
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
